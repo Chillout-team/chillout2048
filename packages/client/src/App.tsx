07adee425ec08d179f8716a6cacc87e748f21aad
Rustam Lyatifov <r.lyatifov93@gmail.com>
2023-03-29 18:24:04 +0300

    добавлен common компонент button

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 import './App.scss';
+import Button from './components/common/button/Button';
 
 function App() {
   useEffect(() => {
@@ -12,7 +13,7 @@ function App() {
 
     fetchServerData();
   }, []);
-  return <div className="App">Вот тут будет жить ваше приложение :)</div>;
+  return <Button>button</Button>;
 }
 
 export default App;
