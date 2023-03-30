6cedc5fec71e9f9cd89db815b26a20beafdddf0d
Rustam Lyatifov <r.lyatifov93@gmail.com>
2023-03-30 21:22:01 +0300

    переработка компонента

--- packages/client/src/components/common/input/Input.tsx
+++ packages/client/src/components/common/input/Input.tsx
@@ -1,37 +1,47 @@
-import cls from './Input.module.scss';
-
 interface InputInterface
   extends Omit<React.HTMLProps<HTMLInputElement>, 'size'> {
   id: string;
   value: string;
-  type: 'text' | 'email' | 'password';
+  type?: 'text' | 'email' | 'password';
+  containerClassName?: string;
+  inputClassName?: string;
+  labelClassName?: string;
+  errorClassName?: string;
+  labelText?: string;
   placeholder?: string;
-  success?: boolean;
-  error?: boolean;
   disabled?: boolean;
   errorText?: string;
-  extraClass?: string;
   onChange(e: React.ChangeEvent<HTMLInputElement>): void;
-  onBlur?(e?: React.FocusEvent<HTMLInputElement>): void;
-  onFocus?(e?: React.FocusEvent<HTMLInputElement>): void;
 }
 
 export const Input = (props: InputInterface) => {
-  const { id, error, placeholder, errorText, ...other } = props;
+  const {
+    id,
+    containerClassName,
+    inputClassName,
+    labelClassName,
+    errorClassName,
+    placeholder = ' ',
+    labelText,
+    errorText,
+    ...other
+  } = props;
   return (
-    <div
-      className={
-        error ? cls.input_container + ' ' + cls.__error : cls.input_container
-      }>
-      <input className={cls.input} {...other} placeholder=" " />
+    <div className={containerClassName}>
+      <input
+        className={inputClassName}
+        {...other}
+        placeholder={placeholder}
+        name={id}
+      />
 
-      {placeholder && (
-        <label className={cls.placeholder} htmlFor={id}>
-          {placeholder}
+      {labelText && (
+        <label className={labelClassName} htmlFor={id}>
+          {labelText}
         </label>
       )}
 
-      {errorText && <label className={cls.input_error}>{errorText}</label>}
+      {errorText && <label className={errorClassName}>{errorText}</label>}
     </div>
   );
 };
