343d437d5f13ece354a487792f374443442263ae
Rustam Lyatifov <r.lyatifov93@gmail.com>
2023-03-31 15:57:33 +0300

    clear App

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,7 +1,5 @@
 import { useEffect } from 'react';
 import './App.scss';
-import { Main } from './components/common/main/Main';
-import { Header } from './components/common/header/Header';
 
 function App() {
   useEffect(() => {
@@ -14,31 +12,7 @@ function App() {
 
     fetchServerData();
   }, []);
-  return (
-    <>
-      <Header>
-        <nav>
-          <ul>
-            <li>Форум</li>
-            <li>Рейтинг</li>
-          </ul>
-        </nav>
-        <nav>
-          <ul>
-            <li>
-              <div></div>
-            </li>
-            <li>Зарегистрироваться</li>
-            <li>Войти</li>
-          </ul>
-        </nav>
-      </Header>
-      <Main>
-        <div>1</div>
-        <div>2</div>
-      </Main>
-    </>
-  );
+  return <div className="App">Вот тут будет жить ваше приложение :)</div>;
 }
 
 export default App;
