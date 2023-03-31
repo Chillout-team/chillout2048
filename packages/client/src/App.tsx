c97bab257bdb63a655235ee2f1d3cb416e5c9a43
Rustam Lyatifov <r.lyatifov93@gmail.com>
2023-03-31 15:55:10 +0300

    CHI-40 создания компонента main, header + обнуляющие стили

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,5 +1,7 @@
 import { useEffect } from 'react';
 import './App.scss';
+import { Main } from './components/common/main/Main';
+import { Header } from './components/common/header/Header';
 
 function App() {
   useEffect(() => {
@@ -12,7 +14,31 @@ function App() {
 
     fetchServerData();
   }, []);
-  return <div className="App">Вот тут будет жить ваше приложение :)</div>;
+  return (
+    <>
+      <Header>
+        <nav>
+          <ul>
+            <li>Форум</li>
+            <li>Рейтинг</li>
+          </ul>
+        </nav>
+        <nav>
+          <ul>
+            <li>
+              <div></div>
+            </li>
+            <li>Зарегистрироваться</li>
+            <li>Войти</li>
+          </ul>
+        </nav>
+      </Header>
+      <Main>
+        <div>1</div>
+        <div>2</div>
+      </Main>
+    </>
+  );
 }
 
 export default App;
---
+++ packages/client/src/components/common/main/Main.tsx
@@ -0,0 +1,11 @@
+import cls from './Main.module.scss';
+
+type Props = {
+  extraClass?: string;
+  children?: React.ReactNode;
+};
+
+export const Main = (props: Props) => {
+  const { children, extraClass = '' } = props;
+  return <main className={cls.main + ' ' + extraClass}>{children}</main>;
+};
