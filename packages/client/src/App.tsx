5735493ea2f93e756e988a1b8604215fd0581930
Rustam Lyatifov <r.lyatifov93@gmail.com>
2023-04-01 18:09:16 +0300

    return

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -9,7 +9,6 @@ function App() {
             const data = await response.json();
             console.log(data);
         };
-
         fetchServerData();
     }, []);
     return <div className="App">Вот тут будет жить ваше приложение :)</div>;
---
+++ packages/client/src/components/registration/Registration.tsx
@@ -0,0 +1,10 @@
+import { Main } from "../common/main/Main";
+import { RegistrationForm } from "./registrationForm/RegistrationForm";
+
+export const Registration = () => {
+    return (
+        <Main>
+            <RegistrationForm />
+        </Main>
+    );
+};
