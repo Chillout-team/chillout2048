0dbc1209a1da96be7e47fe4721937d0df4eee4ec
Lyatifov <r.lyatifov93@gmail.com>
2023-04-12 15:14:54 +0300

    игра перенесана на страницу game

--- packages/client/src/features/game/Game.tsx
+++ packages/client/src/features/game/Game.tsx
@@ -1,3 +1,34 @@
+import { Main } from "../../components/common/main/Main";
+import { Header } from "../../components/common/header/Header";
+import cls from "./Game.module.scss";
+import { GameLoad } from "@/utils/game/Game";
+import { useEffect } from "react";
+
 export const Game = () => {
-    return <></>;
+    useEffect(() => {
+        const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+        GameLoad(canvas);
+    }, []);
+
+    return (
+        <>
+            <Header isAuth={false} />
+            <Main>
+                <div className={cls.main_page_wrapper}>
+                    <h1 className={cls.header}># 2048</h1>
+                    <p className={cls.header_text}>
+                        <span>Как играть:</span> Передвигая плитки, нужно
+                        сложить кубики одного «номинала».
+                        <br /> Целью игры является получение плитки номинала
+                        «2048».
+                    </p>
+                    <canvas id="canvas" className={cls.canvas}></canvas>
+                    <p className={cls.text}>
+                        Проверьте на сколько вы удачливый и весело проведите
+                        свое свободное время.
+                    </p>
+                </div>
+            </Main>
+        </>
+    );
 };
--- packages/client/src/features/home/Home.tsx
+++ packages/client/src/features/home/Home.tsx
@@ -1,47 +1,40 @@
-import { FC, useEffect } from "react";
+import { FC } from "react";
 import { Main } from "../../components/common/main/Main";
 import { Header } from "../../components/common/header/Header";
 import { Button } from "../../components/common/button/Button";
-// import { GamePic } from "../../assets/img/GamePic";
+import { GamePic } from "../../assets/img/GamePic";
 import cls from "./Home.module.scss";
 import { Link } from "react-router-dom";
 import { ROUTES } from "@/router/routes";
-import { GameLoad } from "@/utils/game/Game";
 
 export const Home: FC = () => {
-    useEffect(() => {
-        const canvas = document.getElementById("canvas") as HTMLCanvasElement;
-        GameLoad(canvas);
-    }, []);
-
     return (
-        <Main>
+        <>
             <Header isAuth={false} />
-            <div className={cls.main_page_wrapper}>
-                <h1 className={cls.header}># 2048</h1>
-                <p className={cls.header_text}>
-                    <span>Как играть:</span> Передвигая плитки, нужно сложить
-                    кубики одного «номинала».
-                    <br /> Целью игры является получение плитки номинала «2048».
-                </p>
-                <Link to={ROUTES.GAME.path}>
-                    <Button
-                        size="medium"
-                        color="orange"
-                        type="submit"
-                        onClick={() => {
-                            return;
-                        }}
-                        children="Начать игру!"
-                    />
-                </Link>
-                <canvas id="canvas" className={cls.canvas}></canvas>
-
-                <p className={cls.text}>
-                    Проверьте на сколько вы удачливый и весело проведите свое
-                    свободное время.
-                </p>
-            </div>
-        </Main>
+            <Main>
+                <div className={cls.main_page_wrapper}>
+                    <h1 className={cls.header}># 2048</h1>
+                    <p className={cls.header_text}>
+                        <span>Как играть:</span> Передвигая плитки, нужно
+                        сложить кубики одного «номинала».
+                        <br /> Целью игры является получение плитки номинала
+                        «2048».
+                    </p>
+                    <Link to={ROUTES.GAME.path}>
+                        <Button
+                            size="medium"
+                            color="orange"
+                            type="submit"
+                            children="Начать игру!"
+                        />
+                    </Link>
+                    <GamePic className={cls.img} />
+                    <p className={cls.text}>
+                        Проверьте на сколько вы удачливый и весело проведите
+                        свое свободное время.
+                    </p>
+                </div>
+            </Main>
+        </>
     );
 };
