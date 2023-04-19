0e86ec908d5e8b7f2e74ee0d6d4f0041c330bcbf
Mary <mevdokimova@innodata.ru>
2023-04-19 14:17:18 +0300

    Интерфейсы именуются с I. Убраны лишние скобки

--- packages/client/src/components/common/errorBoundary/ErrorBoundary.tsx
+++ packages/client/src/components/common/errorBoundary/ErrorBoundary.tsx
@@ -1,26 +1,26 @@
 import React, { ReactNode } from "react";
 import { ErrorPage } from "@/features/errorPage/ErrorPage";
 
-interface Props {
+interface IProps {
     children?: ReactNode;
 }
 
-interface State {
+interface IState {
     hasError: boolean;
 }
 
-export class ErrorBoundary extends React.Component<Props, State> {
-    public state: State = {
+export class ErrorBoundary extends React.Component<IProps, IState> {
+    public state: IState = {
         hasError: false,
     };
 
-    public static getDerivedStateFromError(): State {
+    public static getDerivedStateFromError(): IState {
         return { hasError: true };
     }
 
     public render() {
         if (this.state.hasError) {
-            return <ErrorPage type={"500"} />;
+            return <ErrorPage type="500" />;
         }
         return this.props.children;
     }
