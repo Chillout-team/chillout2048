964f0505dd7e2bfe402ae527f5ef0547937aea73
Кургузов Кирилл <kskurguzov@sberbank.ru>
2023-04-22 19:28:10 +0300

    CHI-55 Авторизация должна проверяться через отдельно написанный Hook + bugfix: user already in system - после выхода пользователя

--- packages/client/src/components/common/header/Header.tsx
+++ packages/client/src/components/common/header/Header.tsx
@@ -1,57 +1,38 @@
 import { ROUTES } from "@/router/routes";
 import cls from "./Header.module.scss";
 import { Link } from "react-router-dom";
-import { useSelector } from "react-redux";
-import { RootState } from "@/redux/store";
-import { useAppDispatch } from "@/redux/hooks";
-import { logout } from "@/redux/actions/authAction";
+import { useAuthorization } from "@/hooks/useAuthorization";
 
 type Props = {
     extraClass?: string;
 };
 
 export const Header = ({ extraClass = "" }: Props) => {
-    const avatar = useSelector(
-        (state: RootState) => state.user?.user?.avatar || "",
-    );
-    const login = useSelector(
-        (state: RootState) => state.user?.user?.login || "",
-    );
-    const dispatch = useAppDispatch();
+    const {
+        userData: { avatar, login },
+        isAuthorized,
+        handleLogout,
+    } = useAuthorization();
 
-    const handleLogout = () => dispatch(logout());
+    const renderSignMenu = isAuthorized ? (
+        <div>
+            <Link to={ROUTES.PROFILE.path} className={cls.link_user}>
+                <div>
+                    <img src={avatar} alt="Avatar" width="34" height="34" />
+                </div>
+                {login}
+            </Link>
+            <Link to={ROUTES.HOME.path} onClick={handleLogout}>
+                Выйти
+            </Link>
+        </div>
+    ) : (
+        <div>
+            <Link to={ROUTES.SINGUP.path}>Зарегистрироваться</Link>
+            <Link to={ROUTES.SINGIN.path}>Войти</Link>
+        </div>
+    );
 
-    if (login) {
-        return (
-            <header className={`${cls.header} ${extraClass}`}>
-                <nav>
-                    <div>
-                        <Link to={ROUTES.HOME.path}>Домой</Link>
-                        <Link to={ROUTES.FORUM.path}>Форум</Link>
-                        <Link to={ROUTES.LEADERBOARD.path}>Рейтинг</Link>
-                    </div>
-                    <div>
-                        <Link
-                            to={ROUTES.PROFILE.path}
-                            className={cls.link_user}>
-                            <div>
-                                <img
-                                    src={avatar}
-                                    alt="Avatar"
-                                    width="34"
-                                    height="34"
-                                />
-                            </div>
-                            {login}
-                        </Link>
-                        <Link to={ROUTES.HOME.path} onClick={handleLogout}>
-                            Выйти
-                        </Link>
-                    </div>
-                </nav>
-            </header>
-        );
-    }
     return (
         <header className={`${cls.header} ${extraClass}`}>
             <nav>
@@ -60,10 +41,7 @@ export const Header = ({ extraClass = "" }: Props) => {
                     <Link to={ROUTES.FORUM.path}>Форум</Link>
                     <Link to={ROUTES.LEADERBOARD.path}>Рейтинг</Link>
                 </div>
-                <div>
-                    <Link to={ROUTES.SINGUP.path}>Зарегистрироваться</Link>
-                    <Link to={ROUTES.SINGIN.path}>Войти</Link>
-                </div>
+                {renderSignMenu}
             </nav>
         </header>
     );
