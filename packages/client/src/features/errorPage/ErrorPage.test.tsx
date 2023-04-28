6557139f69222209aa4650203d07c14eb5a07667
Mary <mevdokimova@innodata.ru>
2023-04-28 10:17:48 +0300

    add ssr

--- packages/client/src/features/errorPage/ErrorPage.test.tsx
+++ packages/client/src/features/errorPage/ErrorPage.test.tsx
@@ -1,6 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import "@testing-library/jest-dom/extend-expect";
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
 import { ErrorPage } from "../errorPage/ErrorPage";
 import { BrowserRouter } from "react-router-dom";
 
