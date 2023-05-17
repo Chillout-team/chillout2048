775ec70155ca42fd509515bcb0f3c975b6f6b49f
Lyatifov <r.lyatifov93@gmail.com>
2023-05-17 18:53:57 +0300

    fixed

--- packages/client/src/api/oAuth.ts
+++ packages/client/src/api/oAuth.ts
@@ -4,12 +4,16 @@ const REDIRECT_URI = "http://localhost:3000/oauth";
 
 export const oAuth = {
     singin: async () => {
-        const res = await baseAPI.get(
-            `/oauth/yandex/service-id?redirect_uri=${REDIRECT_URI}`,
-        );
-        const { service_id } = res.data;
-        const URL = `https://oauth.yandex.ru/authorize?response_type=code&client_id=${service_id}&redirect_uri=${REDIRECT_URI}`;
-        window.location.href = URL;
+        try {
+            const res = await baseAPI.get(
+                `/oauth/yandex/service-id?redirect_uri=${REDIRECT_URI}`,
+            );
+            const { service_id } = res.data;
+            const URL = `https://oauth.yandex.ru/authorize?response_type=code&client_id=${service_id}&redirect_uri=${REDIRECT_URI}`;
+            window.location.href = URL;
+        } catch (error) {
+            console.log(error);
+        }
     },
 
     takeToken: async (code: string) => {
@@ -24,7 +28,7 @@ export const oAuth = {
             );
             return res;
         } catch (error) {
-            return;
+            return null;
         }
     },
 };
