34c4e3939525cba973420c2e0ce5b6e8bb873c20
nikita <nikita.lavrentyev@mysky.com>
2023-05-28 17:29:52 +0400

    1

--- packages/client/src/features/forum/emoji/emojiButton/EmojiButton.tsx
+++ packages/client/src/features/forum/emoji/emojiButton/EmojiButton.tsx
@@ -1,5 +1,5 @@
 import { EmojiList } from "../EmojiList";
-import cls from "./emojiButton.module.scss";
+// import cls from "./emojiButton.module.scss";
 import { FC } from "react";
 import { Dispatch } from "react";
 
@@ -14,12 +14,12 @@ export const EmojiButton: FC<Props> = ({ text, setText }) => {
     };
 
     return (
-        <div className={cls.emojiWrapper}>
+        <div >
             {EmojiList.map((emoji, index) => {
                 return (
                     <button
                         key={index}
-                        className={cls.emoji}
+                        // className={cls.emoji}
                         type="button"
                         onClick={() => addEmoji(emoji)}>
                         {emoji}
